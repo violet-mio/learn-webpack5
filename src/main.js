711ca279287427c24140db39594d40b5dd6ca159
violet-mio <caohai648@qq.com>
2022-05-21 23:40:47 +0800

    feat: 手动安装Babel插件

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { introComponent } from './js/css-loader-demo'
 import { PoetryComponent } from './js/less-loader-demo'
 import './js/img-demo'
 import './js/iconfont-demo'
+import { sum } from './js/sum'
 
 document.body.appendChild(introComponent())
 document.body.appendChild(PoetryComponent())
+console.log(sum(233, 100))
