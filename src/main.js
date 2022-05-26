39263e039c755b7234521822fb4f262a7f38c812
violet-mio <caohai648@qq.com>
2022-05-26 16:30:59 +0800

    feat: 打包vue3的template属性定义的字符串模板

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { introComponent } from './js/css-loader-demo'
 import { PoetryComponent } from './js/less-loader-demo'
+// 在构建工具中（webpack，rollup等），import xxx from 'vue'时，默认使用vue.runtime.esm-bundler.js, 不带有编译器功能，无法编译template的内容
+// 所以这里使用vue.esm-bundler版本，带有编译器的才能支持template属性
+import { createApp } from 'vue/dist/vue.esm-bundler'
 import './js/img-demo'
 import './js/iconfont-demo'
 import { sum } from './js/sum'
@@ -7,3 +10,10 @@ import { sum } from './js/sum'
 document.body.appendChild(introComponent())
 document.body.appendChild(PoetryComponent())
 console.log(sum(233, 100))
+
+const app = createApp({
+  template: `
+    <p>这是一段vue内容</p>
+  `
+})
+app.mount('#app')
