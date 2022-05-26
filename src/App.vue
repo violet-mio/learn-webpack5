a676e9dcb3cddbecde3dda4f65e7b77eb91c9e1b
violet-mio <caohai648@qq.com>
2022-05-26 19:07:04 +0800

    feat: 配置webpack的watch模式

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
     <p>这是是单文件组件的内容：{{msg}}</p>
+    <p>{{info}}</p>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 const msg = ref('Hello Vue3')
+const info = ref('Today is good!')
 
 </script>
